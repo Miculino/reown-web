cb1b8abd14797feb5cd43bb9ef09b8701a30ecca
RemusDB <remusd.buhaianu@gmail.com>
2024-11-12 13:51:55 +0200

    chore: create constant for product overview list

---
+++ app/constants/productOverviewList.ts
@@ -0,0 +1,17 @@
+export const PRODUCT_OVERVIEW_LIST = [
+  {
+    title: "UX native",
+    description:
+      "With utility and usability at the core of our kits. Create designs that feel natural, functional, and user-centric.",
+  },
+  {
+    title: "Unlimited scalability",
+    description:
+      "Unlock instant connectivity for millions of users across 6000+ apps, 500+ wallets, and 150+ chains",
+  },
+  {
+    title: "Complete control",
+    description:
+      "Leverage advanced feature stacks and flexible customization tools to create the perfect integration for your project.",
+  },
+];
