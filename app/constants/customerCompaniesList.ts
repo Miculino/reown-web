29bfe99b520738b5ea83faf39756cbe9030339e7
RemusDB <remusd.buhaianu@gmail.com>
2024-11-12 23:46:23 +0200

    chore: add social proof constant list

---
+++ app/constants/customerCompaniesList.ts
@@ -0,0 +1,30 @@
+export const CUSTOMER_COMPANIES_LIST = [
+  {
+    name: "ATFKT",
+    logo: "",
+  },
+  {
+    name: "zerion",
+  },
+  {
+    name: "zkSync",
+  },
+  {
+    name: "Uniswap",
+  },
+  {
+    name: "PancakeSwap",
+  },
+  {
+    name: "Fireblocks",
+  },
+  {
+    name: "Metamask",
+  },
+  {
+    name: "OKX",
+  },
+  {
+    name: "[Ledger]",
+  },
+];
