b73548f733b230d785418ae4c924c4690f14ef97
RemusDB <remusd.buhaianu@gmail.com>
2024-11-13 19:28:16 +0200

    feat(onchain platform): responsive styling for mobile

--- app/(site)/layout/onchain-cta/OnchainCTA.tsx
+++ app/(site)/layout/onchain-cta/OnchainCTA.tsx
@@ -4,34 +4,40 @@ import styles from "./onchain-cta.module.scss";
 // Components
 import Button from "../../components/Button";
 
-export default function Hero() {
+// CLSX
+import clsx from "clsx";
+
+export default function OnchainCTA() {
   return (
     <section>
       <div
-        className={`rounded-[100px] xl:rounded-full relative h-[85vh] overflow-hidden ${styles.hero__video_container}`}
+        className={`rounded-[100px] mt-16 bg-dark-gray xl:rounded-full relative h-[85vh] overflow-hidden ${styles.onchain_cta__container}`}
       >
-        <video
-          className="absolute h-full w-full object-cover object-center"
-          src="/videos/Hero.webm"
-          loop
-          autoPlay
-          muted
-        />
         <div
-          className={`grid grid-cols-12 h-full w-full relative z-10 ${styles.hero__grid}`}
+          className={`grid grid-cols-12 h-full w-full relative z-10 ${styles.onchain_cta__grid}`}
         >
-          <div className={`flex flex-col gap-4 ${styles.hero__grid_content}`}>
-            <h1 className="text-lg md:text-3xl text-white">
-              Toolkits to build onchain UX
-            </h1>
+          <div className={clsx(styles.onchain_cta__headline, "flex")}>
+            <div className="my-auto sm:w-[25em]">
+              <h2 className="text-4xl text-white">
+                Building the onchain UX platform
+              </h2>
+            </div>
+          </div>
+          <div className={`flex flex-col gap-4 ${styles.onchain_cta__content}`}>
+            <h1 className="text-lg  text-white">Build with reown.</h1>
             <p className="text-[#BBBBBB] text-base max-w-80 ml-8">
-              Reown gives developers the tools to build user experiences that
-              make digital ownership effortless, intuitive, and secure.
+              Make digital ownership effortless, intuitive, and secure. Start
+              building with Reown and create unparalleled user experiences
+              today.
             </p>
             <div className="flex items-center gap-1">
-              <Button href="https://cloud.reown.com/app">Start building</Button>
+              <Button href="https://cloud.reown.com/app">Build for free</Button>
 
-              <Button intent={"outline"} href="https://docs.reown.com">
+              <Button
+                color={"blue"}
+                intent={"outline"}
+                href="https://docs.reown.com"
+              >
                 Docs
               </Button>
             </div>
