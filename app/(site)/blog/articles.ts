68253936490919cd4defb1a9b2cdbfa96c79eae0
RemusDB <remusd.buhaianu@gmail.com>
2024-11-13 17:55:15 +0200

    chore: add blog articles static data

---
+++ app/(site)/blog/articles.ts
@@ -0,0 +1,17 @@
+export const articles = [
+  {
+    date: "09.16.2024",
+    title: "Meet Reown: the onchain UX platform",
+    category: "Company update",
+  },
+  {
+    date: "11.11.2024",
+    title: "Enhancing Cross-Chain Interactions: Reown’s Phased...",
+    category: "WalletKit",
+  },
+  {
+    date: "11.11.2024",
+    title: "From Telegram to Bitcoin – here’s what’s in store with...",
+    category: "AppKit",
+  },
+];
