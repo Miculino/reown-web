4f5587953a3f49aaf60a9677fccb6f0b338997b1
RemusDB <remusd.buhaianu@gmail.com>
2024-11-13 10:07:29 +0200

    fix: remove unsued name prop

--- app/(site)/layout/social-proof/SocialProof.tsx
+++ app/(site)/layout/social-proof/SocialProof.tsx
@@ -10,12 +10,12 @@ export default function SocialProof() {
   return (
     <section className="mt-2 overflow-hidden rounded-[40px] bg-gray-300">
       <div className={`flex gap-32 p-10 ${styles.customers__container}`}>
-        {CUSTOMER_COMPANIES_LIST.map(({ logo: CompanyLogo, name }, index) => (
+        {CUSTOMER_COMPANIES_LIST.map(({ logo: CompanyLogo }, index) => (
           <div key={index + CUSTOMER_COMPANIES_LIST.length}>
             <CompanyLogo />
           </div>
         ))}
-        {CUSTOMER_COMPANIES_LIST.map(({ logo: CompanyLogo, name }, index) => (
+        {CUSTOMER_COMPANIES_LIST.map(({ logo: CompanyLogo }, index) => (
           <div key={index + CUSTOMER_COMPANIES_LIST.length}>
             <CompanyLogo />
           </div>
