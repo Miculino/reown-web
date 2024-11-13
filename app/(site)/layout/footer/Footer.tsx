cce4ce276932d278412c2867bff117973b049872
RemusDB <remusd.buhaianu@gmail.com>
2024-11-13 19:29:55 +0200

    feat(footer): create footer section

---
+++ app/(site)/layout/footer/Footer.tsx
@@ -0,0 +1,9 @@
+// Styles
+import styles from "./footer.module.scss";
+
+// CLSX
+import clsx from "clsx";
+
+export default function Footer() {
+  return <footer className={clsx(styles.footer)}>Footer</footer>;
+}
