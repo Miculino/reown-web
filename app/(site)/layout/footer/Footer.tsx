0a7874569f17686d4b2d3c87d3cc01c0aa53b229
RemusDB <remusd.buhaianu@gmail.com>
2024-11-13 19:58:29 +0200

    feat(footer): add footer lotties

--- app/(site)/layout/footer/Footer.tsx
+++ app/(site)/layout/footer/Footer.tsx
@@ -11,6 +11,12 @@ export default function Footer() {
   return (
     <footer className={clsx(styles.footer, "mt-2")}>
       <Newsletter />
+      <div className="p-2 rounded-[32px] bg-orange aspect-sqquare">
+        <video src="/videos/Reown_Appkit.webm" autoPlay muted></video>
+      </div>
+      <div className="p-2 rounded-[32px] bg-yellow aspect-square">
+        <video src="/videos/Reown_Walletkit.webm" autoPlay muted></video>
+      </div>
     </footer>
   );
 }
