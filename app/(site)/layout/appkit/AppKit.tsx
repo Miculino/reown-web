4647d540b6eb80efb3b6dc1a9b0c30126c6a91e5
RemusDB <remusd.buhaianu@gmail.com>
2024-11-13 11:09:23 +0200

    feat(product suite): add walletkit and appkit sections

---
+++ app/(site)/layout/appkit/AppKit.tsx
@@ -0,0 +1,16 @@
+export default function AppKit() {
+  return (
+    <section>
+      <div>{/* Mobile Preview */}</div>
+      <div>
+        {/* Card */}
+        <span>Product</span>
+        <h3>The toolkit to build onchain app UX</h3>
+        <p>
+          Build world-class app experiences with multiple, cutting edge features
+          designed to make web3 usable.
+        </p>
+      </div>
+    </section>
+  );
+}
---
+++ app/(site)/layout/walletkit/WalletKit.tsx
@@ -0,0 +1,16 @@
+export default function WalletKit() {
+  return (
+    <section>
+      <div>{/* Mobile Preview */}</div>
+      <div>
+        {/* Card */}
+        <span>Product</span>
+        <h3>The industry standard for designing innovative wallet UX</h3>
+        <p>
+          Unlock native UX features within your wallet and enable wallet users
+          to easily connect with thousands of apps.
+        </p>
+      </div>
+    </section>
+  );
+}
