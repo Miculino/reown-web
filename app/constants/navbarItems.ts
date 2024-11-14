53d166ece23f848bd419f7546ec86af9cc157863
RemusDB <remusd.buhaianu@gmail.com>
2024-11-14 08:19:54 +0200

    chore: add navbar items constant

---
+++ app/constants/navbarItems.ts
@@ -0,0 +1,41 @@
+export const NAVBAR_ITEMS = [
+  {
+    title: "Products",
+    links: [
+      {
+        label: "AppKit",
+        url: "https://reown.com/appkit",
+      },
+      {
+        label: "WalletKit",
+        url: "https://reown.com/walletkit",
+      },
+    ],
+  },
+  {
+    title: "Developer",
+    links: [
+      {
+        label: "Cloud",
+        url: "https://reown.com/cloud",
+      },
+      {
+        label: "Docs",
+        url: "https://docs.reown.com",
+      },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      {
+        label: "About Us",
+        url: "https://reown.com/about-reown",
+      },
+      {
+        label: "Blog",
+        url: "https://reown.com/blog",
+      },
+    ],
+  },
+];
