a61d2273278d2212735935334d591e45f1e7fa86
RemusDB <remusd.buhaianu@gmail.com>
2024-11-14 12:39:07 +0200

    feat(onchain): add responsive design for lottie grid and section

--- app/(site)/layout/onchain-cta/OnchainCTA.tsx
+++ app/(site)/layout/onchain-cta/OnchainCTA.tsx
@@ -3,6 +3,7 @@ import styles from "./onchain-cta.module.scss";
 
 // Components
 import Button from "../../components/Button";
+import GridLottie from "../../components/grid-lottie/GridLottie";
 
 // CLSX
 import clsx from "clsx";
@@ -11,10 +12,13 @@ export default function OnchainCTA() {
   return (
     <section>
       <div
-        className={`rounded-[100px] mt-16 bg-dark-gray xl:rounded-full relative h-[85vh] overflow-hidden ${styles.onchain_cta__container}`}
+        className={`rounded-[100px] p-8 mt-16 bg-dark-gray xl:rounded-full relative overflow-hidden ${styles.onchain_cta__container}`}
       >
+        <div className={clsx(styles.onchain_cta__lottie)}>
+          <GridLottie />
+        </div>
         <div
-          className={`grid grid-cols-12 h-full w-full relative z-10 ${styles.onchain_cta__grid}`}
+          className={`grid grid-cols-6 mt-16 md:mt-0 md:grid-cols-12 h-full w-full relative z-10 ${styles.onchain_cta__grid}`}
         >
           <div className={clsx(styles.onchain_cta__headline, "flex")}>
             <div className="my-auto sm:w-[25em]">
@@ -23,7 +27,9 @@ export default function OnchainCTA() {
               </h2>
             </div>
           </div>
-          <div className={`flex flex-col gap-4 ${styles.onchain_cta__content}`}>
+          <div
+            className={`flex flex-col mt-4 gap-4 ${styles.onchain_cta__content}`}
+          >
             <h1 className="text-lg  text-white">Build with reown.</h1>
             <p className="text-[#BBBBBB] text-base max-w-80 ml-8">
               Make digital ownership effortless, intuitive, and secure. Start
