c06e5d059ee9565553a3610099daf76a97828c0c
RemusDB <remusd.buhaianu@gmail.com>
2024-11-14 15:55:41 +0200

    feat(social proof): adjust social proof carousel

--- app/(site)/layout/social-proof/SocialProof.tsx
+++ app/(site)/layout/social-proof/SocialProof.tsx
@@ -9,14 +9,20 @@ import { CUSTOMER_COMPANIES_LIST } from "@/app/constants/customerCompaniesList";
 export default function SocialProof() {
   return (
     <section className="mt-2 overflow-hidden rounded-[40px] bg-gray-300">
-      <div className={`flex p-10 ${styles.customers__container}`}>
+      <div className={`flex ${styles.customers__container}`}>
         {CUSTOMER_COMPANIES_LIST.map(({ logo: CompanyLogo }, index) => (
-          <div className="px-20" key={index + CUSTOMER_COMPANIES_LIST.length}>
-            <CompanyLogo />
+          <div
+            className="w-52 py-12"
+            key={index + CUSTOMER_COMPANIES_LIST.length}
+          >
+            <CompanyLogo className="w-full h-full" />
           </div>
         ))}
         {CUSTOMER_COMPANIES_LIST.map(({ logo: CompanyLogo }, index) => (
-          <div className="px-20" key={index + CUSTOMER_COMPANIES_LIST.length}>
+          <div
+            className="w-52 py-12"
+            key={index + CUSTOMER_COMPANIES_LIST.length}
+          >
             <CompanyLogo />
           </div>
         ))}
